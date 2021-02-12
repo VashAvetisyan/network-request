f9aa15836f5d0c25e939df20ae339be8509add6f
Your Name <you@example.com>
2021-02-12 14:14:25 +0400

    finish

--- src/containers/Posts/Posts.js
+++ src/containers/Posts/Posts.js
@@ -6,20 +6,22 @@ import service, { getAllPosts } from 'api/service'
 
 import './Posts.scss'
 
+const limit = 9;
+
 export class Posts extends Component {
     state = {
-        posts: []
+        posts: null,
+        start: 0,
+        hasMore: true,
+        loading: false
     }
     
     componentDidMount() {
-        service.getAllPosts()
+        service.getPosts(this.state.start, limit)
             .then(data => {
                 this.setState({
-                    posts: data
+                    posts: data,
                 })
-            })
-            .catch(err => {
-                throw new Error(`Error ${err}`)
             })
     }
 
@@ -52,20 +54,60 @@ export class Posts extends Component {
             })
     }
 
+    deletePost = (id) => {
+        service.deletePost(id)
+            .then(data => {
+                this.setState({
+                    posts: this.state.posts.filter((el) => {
+                        return el.id !== id
+                    })
+                })
+            })
+    }
+
+    getMore = () => {
+        const newStart = this.state.start + limit
+        this.setState({
+            start: newStart,
+            loading: true
+        })
+        service.getPosts(newStart, limit)
+            .then(data => {
+                this.setState({
+                    posts: [...this.state.posts, ...data],
+                    hasMore: data.length < limit ? false : true,
+                    loading: false
+                })
+            })
+    }
+
     render() {
+        const {loading, hasMore, posts} = this.state
         return (
             <div className="app-posts">
-                {
-                    this.state.posts.map(post => {
-                        return <Post
-                            key={post.id}
-                            post={post}
-                            className="app-posts__post"
-                        />
-                    })
-                }
-                <button onClick={this.updataPost}>Update Post</button>
-                <button onClick={this.createPost}>Create Post</button>
+                {posts ? (
+                    <>
+                        <div className="app-posts__container">
+                        {
+                            posts.map(post => {
+                                return <Post
+                                    key={post.id}
+                                    post={post}
+                                    className="app-posts__container__post"
+                                />
+                            })
+                        }
+                        </div>
+                        <div className="app-posts__buttons">
+                            {hasMore &&  <button onClick={this.getMore} disabled={loading}>{loading ? 'Loading...' : 'Get More'}</button>}
+                            <button onClick={this.updataPost}>Update Post</button>
+                            <button onClick={this.createPost}>Create Post</button>
+                            <button onClick={() => this.deletePost(5)}>Delete Post</button>
+                        </div>
+                </>
+                ) : (
+                    <div>Loagind...</div>
+                )}
             </div>
         )
     }
